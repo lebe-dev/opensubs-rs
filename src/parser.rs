d59b83114473a5201cff58b5401913c8fd3cba8a
Eugene Lebedev <duke.tougu@gmail.com>
2020-10-12 23:17:56 +0300

    [~] refactoring

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,6 @@
 pub mod parser {
     use regex::Regex;
-    use scraper::{Html, Selector};
+    use scraper::{ElementRef, Html, Selector};
 
     use crate::domain::domain::{SubtitleSearchResultItem, SubtitleSearchResults};
     use crate::error::error::OperationError;
@@ -33,83 +33,19 @@ pub mod parser {
                         let rows_selector = Selector::parse("tr").unwrap();
 
                         for row in table_body.select(&rows_selector) {
-                            trace!("---[ROW]---");
-                            trace!("{}", row.html());
-                            trace!("---[/ROW]---");
 
-                            let mut details_page_url: &str = "";
-
-                            match row.select(&title_col_selector).skip(1).next() {
-                                Some(title_col) => {
-                                    match title_col.select(&title_details_url_selector).next() {
-                                        Some(a_element) => {
-                                            match a_element.value().attr("href") {
-                                                Some(href) => details_page_url = href,
-                                                None => {}
-                                            }
-                                        }
-                                        None => {}
-                                    }
-
-                                    let title_row = strip_html_tags(&title_col.inner_html());
-
-                                    let title_parts: Vec<&str> = title_row.split("\n").collect();
-
-                                    let title = title_parts.get(0).unwrap();
-                                    let year_part = *title_parts.get(1).unwrap();
-
-                                    let mut year = String::from(year_part);
-
-                                    if year_pattern.is_match(year_part) {
-                                        let groups = year_pattern.captures_iter(year_part).next().unwrap();
-
-                                        year = String::from(&groups[1]);
-                                    }
-
-                                    let merged_title = format!("{} ({})", title, year);
-
-                                    info!("year '{}'", year_part);
-
-                                    let mut season: u16 = 0;
-                                    let mut episode: u16 = 0;
-
-                                    match title_parts.get(2) {
-                                        Some(series_info) => {
-                                            info!("series info '{}'", series_info);
-
-                                            if series_pattern.is_match(series_info) {
-                                                let groups = series_pattern.captures_iter(series_info).next().unwrap();
-
-                                                match String::from(&groups[1]).parse() {
-                                                    Ok(value) => season = value,
-                                                    Err(e) =>
-                                                        error!("unable to get season value: {}", e)
-                                                }
-
-                                                match String::from(&groups[2]).parse() {
-                                                    Ok(value) => episode = value,
-                                                    Err(e) =>
-                                                        error!("unable to get episode value: {}", e)
-                                                }
-                                            }
-                                        }
-                                        None => debug!("no series info found")
-                                    }
-
-                                    let search_result_item = SubtitleSearchResultItem {
-                                        index: row_index,
-                                        title: merged_title.to_string(),
-                                        details_url: details_page_url.to_string(),
-                                        season,
-                                        episode
-                                    };
-
+                            match get_search_item_from_row(
+                                row_index, &row, &title_col_selector,
+                                &title_details_url_selector, &year_pattern, &series_pattern
+                            ) {
+                                Ok(search_result_item) => {
                                     results.push(search_result_item);
-
                                     row_index += 1;
-                                }
-                                None => error!("unable to get column with title")
+                                },
+                                Err(e) =>
+                                    error!("unable to extract search result item from row: {}", e)
                             }
+
                         }
                     }
                     None => error!("unable to find table body")
@@ -122,5 +58,91 @@ pub mod parser {
                 Err(OperationError::HtmlParseError)
             }
         }
+    }
+
+    fn get_search_item_from_row(row_index: u16, row: &ElementRef,
+                                title_col_selector: &Selector,
+                                title_details_url_selector: &Selector,
+                                year_pattern: &Regex, series_pattern: &Regex) ->
+                                                 Result<SubtitleSearchResultItem, OperationError> {
+        trace!("---[ROW]---");
+        trace!("{}", row.html());
+        trace!("---[/ROW]---");
+
+        let mut result: Result<SubtitleSearchResultItem, OperationError> = Err(OperationError::Error);
+
+        let mut details_page_url: &str = "";
+
+        match row.select(&title_col_selector).skip(1).next() {
+            Some(title_col) => {
+                match title_col.select(&title_details_url_selector).next() {
+                    Some(a_element) => {
+                        match a_element.value().attr("href") {
+                            Some(href) => details_page_url = href,
+                            None => {}
+                        }
+                    }
+                    None => {}
+                }
+
+                let title_row = strip_html_tags(&title_col.inner_html());
+
+                let title_parts: Vec<&str> = title_row.split("\n").collect();
+
+                let title = title_parts.get(0).unwrap();
+                let year_part = *title_parts.get(1).unwrap();
+
+                let mut year = String::from(year_part);
+
+                if year_pattern.is_match(year_part) {
+                    let groups = year_pattern.captures_iter(year_part).next().unwrap();
+
+                    year = String::from(&groups[1]);
+                }
+
+                let merged_title = format!("{} ({})", title, year);
+
+                info!("year '{}'", year_part);
+
+                let mut season: u16 = 0;
+                let mut episode: u16 = 0;
+
+                match title_parts.get(2) {
+                    Some(series_info) => {
+                        info!("series info '{}'", series_info);
+
+                        if series_pattern.is_match(series_info) {
+                            let groups = series_pattern.captures_iter(series_info).next().unwrap();
+
+                            match String::from(&groups[1]).parse() {
+                                Ok(value) => season = value,
+                                Err(e) =>
+                                    error!("unable to get season value: {}", e)
+                            }
+
+                            match String::from(&groups[2]).parse() {
+                                Ok(value) => episode = value,
+                                Err(e) =>
+                                    error!("unable to get episode value: {}", e)
+                            }
+                        }
+                    }
+                    None => debug!("no series info found")
+                }
+
+                let search_result_item = SubtitleSearchResultItem {
+                    index: row_index,
+                    title: merged_title.to_string(),
+                    details_url: details_page_url.to_string(),
+                    season,
+                    episode
+                };
+
+                result = Ok(search_result_item);
+            }
+            None => error!("unable to get column with title")
+        }
+
+        result
     }
 }
