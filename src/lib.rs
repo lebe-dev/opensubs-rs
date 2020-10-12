50cac3a1a9f2d44d6550e43c7fc65a05f13272b5
Eugene Lebedev <duke.tougu@gmail.com>
2020-10-12 22:21:09 +0300

    [+] add documentation for search_subs_by_mask func

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 extern crate log;
 extern crate log4rs;
 
+use crate::domain::domain::SubtitleSearchResults;
+use crate::error::error::OperationError;
+use crate::search::search::search_by_mask;
+
 mod domain;
 mod parser;
 mod error;
@@ -14,5 +18,24 @@ mod search;
 mod search_tests;
 
 pub const BASE_URL: &str = "https://www.opensubtitles.org";
+
+/// Search subtitles by mask
+///
+/// You must provide `mask` and `sub_langs`.
+///
+/// # Examples
+/// ```
+/// use opensubs_rs::search_subs_by_mask;
+///
+/// // It will search with mask 'Midnight Gospel S01' for Russian and Irish languages
+/// search_subs_by_mask(&client, "Midnight Gospel S01", "rus,gle");
+/// ```
+pub async fn search_subs_by_mask(client: &reqwest::Client, mask: &str, sub_langs: &str) ->
+                                            Result<SubtitleSearchResults, OperationError> {
+    match search_by_mask(&client, BASE_URL, mask, sub_langs).await {
+        Ok(search_results) => Ok(search_results),
+        Err(e) => Err(e)
+    }
+}
 
 
