1e3fe05648b9226d3cddfc676d0433f07f2f4283
Eugene Lebedev <duke.tougu@gmail.com>
2020-10-15 05:43:01 +0300

    [-] cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -151,63 +151,6 @@ fn get_serial_season_search_url(base_url: &str, search_mask: &str,
     )
 }
 
-/// Search subtitles by mask
-///
-/// You must provide `mask` and `sub_langs`, subtitle languages. Example: rus,ara (Russian, Arabic)
-///
-pub async fn search_by_mask(client: &reqwest::Client, base_url: &str,
-                            mask: &str, sub_langs: &str) ->
-                            Result<SubtitleSearchResults, OperationError> {
-    info!("search subtitles by mask '{}'", mask);
-    info!("language '{}'", sub_langs);
-
-    let request_url = get_search_request_url(base_url, mask, sub_langs);
-
-    debug!("request url:");
-    debug!("{}", request_url);
-
-    match client.get(&request_url).send().await {
-        Ok(resp) => {
-            let status: reqwest::StatusCode = resp.status();
-            debug!("server response code: {}", status.as_str());
-
-            if status == reqwest::StatusCode::OK {
-                match resp.text().await {
-                    Ok(response_text) => {
-                        trace!("---[SEARCH RESULTS]---");
-                        trace!("{}", &response_text);
-                        trace!("---[/SEARCH RESULTS]---");
-
-                        match parse_series_search_results(&response_text) {
-                            Ok(search_results) => Ok(search_results),
-                            Err(_) => Err(OperationError::Error)
-                        }
-                    }
-                    Err(e) => {
-                        error!("unable to get response text: {}", e);
-                        Err(OperationError::Error)
-                    }
-                }
-
-            } else { Err(OperationError::Error) }
-        }
-        Err(e) => {
-            error!("subtitles search error: {}", e);
-            Err(OperationError::Error)
-        }
-    }
-}
-
-fn get_search_request_url(base_url: &str, search_mask: &str, language: &str) -> String {
-    let sanitized_mask = search_mask.replace(" ", "+");
-    // https://www.opensubtitles.org/en/search/sublanguageid-rus,eng/moviename-adventure+time/season-2/SearchOnlyTVSeries-on
-    format!("{}/ru/search2?MovieName={}&id=8&action=search&SubLanguageID=rus&SubLanguageID={}\
-                &Season=&Episode=&SubSumCD=&Genre=&MovieByteSize=&MovieLanguage=&\
-                MovieImdbRatingSign=1&MovieImdbRating=&MovieCountry=&MovieYearSign=1&\
-                MovieYear=&MovieFPS=&SubFormat=&SubAddDate=&Uploader=&IDUser=&Translator=&\
-                IMDBID=&MovieHash=&IDMovie=", base_url, sanitized_mask, language)
-}
-
 async fn fetch_and_parse<R>(client: &reqwest::Client, url: &str,
                             parser_func: impl Fn(&str) -> OperationResult<R>) -> OperationResult<R> {
     debug!("request url:");
