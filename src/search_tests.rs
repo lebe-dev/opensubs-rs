bbf3634f74e4ae353279fed2b999208ef57ccd42
Eugene Lebedev <duke.tougu@gmail.com>
2020-10-15 06:08:44 +0300

    [+] add test for 'get download url from page' function; remove unnecessary login operation

--- src/search_tests.rs
+++ src/search_tests.rs
@@ -1,19 +1,9 @@
 #[cfg(test)]
 mod search_tests {
-    use log::LevelFilter;
+    use crate::{BASE_URL, get_download_url_from_page, search_serial_season};
 
-    use crate::{BASE_URL, login, search_serial_season};
-    use crate::test_utils::test_utils::get_logging_config;
-
-    const LOGIN: &str = "CHANGE-ME";
-    const PASSWORD: &str = "CHANGE-ME";
-
-    #[ignore]
     #[tokio::test]
-    async fn result_should_contain_series_search_results() {
-        let logging_config = get_logging_config(LevelFilter::Debug);
-        log4rs::init_config(logging_config).unwrap();
-
+    async fn result_should_contain_relative_url() {
         let client = reqwest::Client::builder()
             .user_agent("Google Chrome")
             .connection_verbose(true)
@@ -21,19 +11,34 @@ mod search_tests {
             .build()
             .unwrap();
 
-        match login(&client, BASE_URL,
-                    LOGIN, PASSWORD).await {
-            Ok(_) => {
-                match search_serial_season(&client, BASE_URL,
-                                     "Midnight Gospel", "rus", 1).await {
-                    Ok(search_results) => {
-                        assert!(search_results.len() > 0);
-                        println!("{:?}", &search_results);
-                    }
-                    Err(_) => panic!("search results expected")
-                }
+        match get_download_url_from_page(
+            &client, "https://www.opensubtitles.org/en/subtitles/8314554/midnight-sun-ko"
+        ).await {
+            Ok(url) =>
+                assert_eq!(
+                    "/en/subtitleserve/sub/8314554",
+                    url.unwrap()
+                ),
+            Err(_) => panic!("search results expected")
+        }
+    }
+
+    #[tokio::test]
+    async fn result_should_contain_series_search_results() {
+        let client = reqwest::Client::builder()
+            .user_agent("Google Chrome")
+            .connection_verbose(true)
+            .cookie_store(true)
+            .build()
+            .unwrap();
+
+        match search_serial_season(&client, BASE_URL,
+                                   "Midnight Gospel", "rus", 1).await {
+            Ok(search_results) => {
+                assert!(search_results.len() > 0);
+                println!("{:?}", &search_results);
             }
-            Err(_) => panic!("auth success expected")
+            Err(_) => panic!("search results expected")
         }
     }
 }
