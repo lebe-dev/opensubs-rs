92d29b5d2b2c4e7e8600c8139edcb7b90426f614
Eugene Lebedev <duke.tougu@gmail.com>
2020-10-15 07:41:14 +0300

    [~] refactoring

--- src/search_tests.rs
+++ src/search_tests.rs
@@ -1,18 +1,14 @@
 #[cfg(test)]
 mod search_tests {
     use log::LevelFilter;
+    use reqwest::Client;
 
     use crate::{BASE_URL, get_download_url_from_page, search_serial_episode, search_serial_season};
     use crate::test_utils::test_utils::get_logging_config;
 
     #[tokio::test]
     async fn search_serial_episode_with_one_result() {
-        let client = reqwest::Client::builder()
-            .user_agent("Google Chrome")
-            .connection_verbose(true)
-            .cookie_store(true)
-            .build()
-            .unwrap();
+        let client = get_client();
 
         match search_serial_episode(
             &client, "https://www.opensubtitles.org",
@@ -26,12 +22,7 @@ mod search_tests {
 
     #[tokio::test]
     async fn search_serial_episode_with_multi_results() {
-        let client = reqwest::Client::builder()
-            .user_agent("Google Chrome")
-            .connection_verbose(true)
-            .cookie_store(true)
-            .build()
-            .unwrap();
+        let client = get_client();
 
         match search_serial_episode(
             &client, "https://www.opensubtitles.org",
@@ -47,12 +38,7 @@ mod search_tests {
 
     #[tokio::test]
     async fn result_should_contain_relative_url() {
-        let client = reqwest::Client::builder()
-            .user_agent("Google Chrome")
-            .connection_verbose(true)
-            .cookie_store(true)
-            .build()
-            .unwrap();
+        let client = get_client();
 
         match get_download_url_from_page(
             &client, "https://www.opensubtitles.org/en/subtitles/8314554/midnight-sun-ko"
@@ -70,12 +56,8 @@ mod search_tests {
     async fn result_should_contain_series_episode_search_results() {
         let logging_config = get_logging_config(LevelFilter::Debug);
         log4rs::init_config(logging_config).unwrap();
-        let client = reqwest::Client::builder()
-            .user_agent("Google Chrome")
-            .connection_verbose(true)
-            .cookie_store(true)
-            .build()
-            .unwrap();
+
+        let client = get_client();
 
         match search_serial_episode(&client, BASE_URL,
                                    "Midnight Gospel", "rus", 1, 2).await {
@@ -89,12 +71,7 @@ mod search_tests {
 
     #[tokio::test]
     async fn result_should_contain_series_season_search_results() {
-        let client = reqwest::Client::builder()
-            .user_agent("Google Chrome")
-            .connection_verbose(true)
-            .cookie_store(true)
-            .build()
-            .unwrap();
+        let client = get_client();
 
         match search_serial_season(&client, BASE_URL,
                                    "Midnight Gospel", "rus", 1).await {
@@ -104,5 +81,14 @@ mod search_tests {
             }
             Err(_) => panic!("search results expected")
         }
+    }
+
+    fn get_client() -> Client {
+        reqwest::Client::builder()
+            .user_agent("Google Chrome")
+            .connection_verbose(true)
+            .cookie_store(true)
+            .build()
+            .unwrap()
     }
 }
